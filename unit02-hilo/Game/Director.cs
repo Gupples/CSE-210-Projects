e22490b51f90392e4bc65ace4d9986dea257be03
Jared Davey <dav15024@byui.edu>
2022-05-11 10:09:25 -0600

    Added Director class

---
+++ unit02-hilo/Game/Director.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace unit02_hilo
+{
+    /// <summary>
+    /// A person who directs the game.
+    /// 
+    /// The Director controls the flow of the game
+    /// </summary>
+    public class Director
+    {
+        
+    }
+}
