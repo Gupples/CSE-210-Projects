298205a27da24de88f6aa3c290bf02733aba2fd5
Jared Davey <dav15024@byui.edu>
2022-05-11 10:34:41 -0600

    Added Hilo() method

--- unit02-hilo/Game/Director.cs
+++ unit02-hilo/Game/Director.cs
@@ -11,6 +11,75 @@ namespace unit02_hilo
     /// </summary>
     public class Director
     {
-        
-    }
+        Card card1;
+        Card card2;
+        bool isPlaying;
+        int score;
+        int totalScore;
+        char guess;
+
+        /// <summary>
+        /// Constructs a new instance of Director.
+        /// Will also initialize a value for the first 
+        /// card.
+        /// 
+        /// </summary>
+        public Director()
+        {
+            card1.Draw();
+        }
+
+        /// <summary>
+        /// Starts the game by running the game's loop.
+        /// 
+        /// </summary>
+
+        public void StartGame()
+        {
+            while (isPlaying)
+            {
+                
+            } // exit (isPlaying) loop
+        } // exit StartGame()
+
+        /// <summary>
+        /// Displays the current card's value and 
+        /// asks for the user's guess.
+        /// </summary>
+        public void Hilo()
+        {
+            string hiloInput = "USER HAS NOT GUESSED YET.";
+            bool validInput = false;
+            while (!validInput)
+            {
+                if (hiloInput.ToLower() == "h")
+                {
+                    guess = 'h';
+                    validInput = true;
+                }
+                else if (hiloInput.ToLower() == "l")
+                {
+                    guess = 'l';
+                    validInput = true;
+                }
+                else
+                {
+                    if (hiloInput != "USER HAS NOT GUESSED YET.")
+                    {
+                        Console.WriteLine($"I'm sorry; '{hiloInput}' was not valid");
+                        Console.WriteLine($"input. Please type 'h' or 'l'. ");
+                    }
+                    Console.WriteLine($"The card is :{card1.value}");
+                    Console.Write("Higher or lower? [h/l] ");
+                    hiloInput = Console.ReadLine();  
+                }
+            } // exit (!validInput) loop
+        } // exit Hilo()
+
+
+
+
+
+
+    } // exit Director class
 }
