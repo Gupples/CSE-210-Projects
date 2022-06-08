49b351ada79d254232fbad013e207bb2f35cd9dd
Jared Davey <dav15024@byui.edu>
2022-06-08 09:24:02 -0600

    Disabled up and down movement

--- unit04-greed/Game/Services/KeyboardService.cs
+++ unit04-greed/Game/Services/KeyboardService.cs
@@ -42,17 +42,7 @@ namespace Unit04.Game.Services
             {
                 dx = 1;
             }
-
-            if (Raylib.IsKeyDown(KeyboardKey.KEY_UP))
-            {
-                dy = -1;
-            }
-
-            if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN))
-            {
-                dy = 1;
-            }
-
+            // Don't include dy; won't move up and down.
             Point direction = new Point(dx, dy);
             direction = direction.Scale(cellSize);
 
